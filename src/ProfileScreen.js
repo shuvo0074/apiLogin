3ea850821da0b95931c7c838485e7f93f2dfb9e5
Tarmim Hoque Shuvo <38061271+shuvo0074@users.noreply.github.com>
2018-07-23 18:10:09 +0600

    data sync ,db done

--- src/ProfileScreen.js
+++ src/ProfileScreen.js
@@ -10,32 +10,75 @@ import {
   ActivityIndicator,
 FlatList,
 } from 'react-native';
+var SQLite = require('react-native-sqlite-storage')
+var db = SQLite.openDatabase({name: 'contactsDB', createFromLocation : "~contacts.db", location: 'Library'});
 export default class ProfileScreen extends Component<{}> {
     static navigationOptions = {
-        title: 'Profile',
+        title: 'Your Contacts',
     }
     constructor (props){
       super(props)
       this.state = ({
         isLoading: false,
         dataSource: null,
+        dbDataSource: null,
       })
+      this.setState({isLoading: true})
+      this._sync()
     }
-    componentDidMount() {
-    
+    _sync=()=>{
+      console.log("Syncing contacts")
       return fetch('https://api.androidhive.info/contacts/')
         .then((response) => response.json())
         .then((responseJson) => {
           this.setState({
             isLoading: false,
-            dataSource: responseJson.contacts
-          }, function() {
-            console.log(" Data Source ",this.state.dataSource)
+            dataSource: responseJson.contacts,
+            dbDataSource: responseJson.contacts,
+          }, ()=> {
+            console.log(" Contact Source ",this.state.dataSource)
+            console.log("Database")
+        db.transaction((tx) => {
+          tx.executeSql('DELETE FROM contacts', [], (tx, results) => {});
+        })
+        db.transaction((tx) => {
+          for (let x=0;x<this.state.dataSource.length;x++){
+            tx.executeSql('INSERT INTO contacts ( id , name , email , address , gender , phoneMobile , phoneHome , phoneOffice ) VALUES ( \'' + this.state.dataSource[x].id +'\' , \''+ this.state.dataSource[x].name +'\' , \''+this.state.dataSource[x].email+'\' , \''+this.state.dataSource[x].address+'\' , \''+this.state.dataSource[x].gender+'\' , \''+this.state.dataSource[x].phone.mobile+'\' , \''+this.state.dataSource[x].phone.home+'\' , \''+this.state.dataSource[x].phone.office+'\' )', [], (tx, results) => {
+              console.log("Query completed",x);
+            })}
+        })
+        this._syncDb()
           });
         })
-        .catch((error) => {
-          console.error("Error in datasource ",error);
+        .catch(() => {
+          console.log("Error in datasource ");
         });
+        
+    }
+    _syncDb=()=>{
+      console.log("Syncing contacts from database")
+      db.transaction((tx) => {
+        tx.executeSql('SELECT * FROM contacts', [], (tx, results) => {
+            var len = results.rows.length;
+            console.log("array len "+ len)
+            if (len>0){
+              for (x=0;x<len;x++){
+                var rec
+                rec.id= results.rows.item(x).id
+                rec.name= results.rows.item(x).name
+                rec.email= results.rows.item(x).email
+                rec.address= results.rows.item(x).address
+                rec.gender= results.rows.item(x).gender
+                rec.phone.mobile= results.rows.item(x).phone.mobile
+                rec.phone.home= results.rows.item(x).phone.home
+                rec.phone.office= results.rows.item(x).phone.office
+                var arr=this.state.dbDataSource
+                arr.push(rec)
+                this.setState({dbDataSource: arr})
+              }
+            }
+          });
+      })
     }
 
     FlatListItemSeparator = () => {
@@ -69,6 +112,17 @@ export default class ProfileScreen extends Component<{}> {
       const {navigate}=this.props.navigation
       return (
         <View style={styles.container} >
+            <TouchableOpacity
+            onPress={()=>{
+              this.setState({isLoading: true})
+              this._sync()
+            }}
+            style={styles.button}
+            >
+              <Text>
+                Sync
+              </Text>
+            </TouchableOpacity>
             <View
             
             style={styles.profileContainer} >
@@ -86,7 +140,7 @@ export default class ProfileScreen extends Component<{}> {
             >
             <FlatList
        
-            data={ this.state.dataSource }
+            data={ this.state.dbDataSource }
             style={{width: "95%"}}
             ItemSeparatorComponent = {this.FlatListItemSeparator}
 
